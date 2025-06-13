6fce208f7d04c168a95da88bdd78c70982063dd5
Hong Junhyoung <bellus77@gmail.com>
2025-06-13 13:34:30 +0900

    test: add tox configuration

--- setup.py
+++ setup.py
@@ -20,10 +20,18 @@ setup(
     url='https://github.com/HongJunhyoung/Snowball',
     packages=find_packages(include=["snowball", "snowball.*"]),
     include_package_data=True,
-    install_requires=['numpy>=1.18.4', 'pandas>=1.0.3', 'tqdm>=4.45.0', 'plotly>=5.1.0', 
-                      'scipy>=1.4.1', 'kaleido>=0.2.1', 'Ipython', 'PyPortfolioOpt', 'scikit-learn'],
+    python_requires=">=3.8",
+    install_requires=[
+        'numpy>=1.21.0',
+        'pandas>=1.3.5',
+        'scipy>=1.7.3',
+        'PyPortfolioOpt>=1.5.1'
+        'tqdm>=4.45.0',
+        'plotly>=5.1.0',
+        'kaleido>=0.2.1',
+        'Ipython'
+    ],
     classifiers=[
-        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
     ],
 )
